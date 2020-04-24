c50d53e9bb3e421e7b5ebdfee67516fde4bcb45f
Mikah Mathews <mikah@outofband.org>
2020-04-24 15:22:34 -0700

    Trying to figure out NPM errors

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
-import { exchange-rate } from './../src/currency-calculator.js';
+import { exchangeRate } from './src/currency-calculator.js';
 
 $(document).ready(function() {
   event.preventDefault();
