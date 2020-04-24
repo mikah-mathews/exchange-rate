835ca89f355a075eb4d0c0224fd63e9275378b68
Mikah Mathews <mikah@outofband.org>
2020-04-24 09:34:07 -0700

    Add API network frame

---
+++ src/currency-calculator.js
@@ -0,0 +1,18 @@
+export exchange-rate {
+  async getUSDExchangeRate(){
+    try {
+      let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD`);
+    }
+    let jsonifiedResponse;
+     if (response.ok && response.status === 200) {
+       jsonifiedResponse = await response.json();
+     } else {
+       jsonifiedResponse = false;
+     }
+     return jsonifiedResponse;
+   } catch (error) {
+     console.log('in the catch');
+     return false;
+   }
+  }
+}
