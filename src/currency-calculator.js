16f5b2e30518f785f61f6d9565bb7cb19c6f137d
Mikah Mathews <mikah@outofband.org>
2020-05-10 23:06:15 -0700

    Get NPM working, add values to collect user input

--- src/currency-calculator.js
+++ src/currency-calculator.js
@@ -1,22 +1,35 @@
-export exchangeRate {
+export class ExchangeRate {
   async getUSDExchangeRate(){
     try {
       let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD/`);
+      let jsonifiedResponse;
+      if (response.ok && response.status === 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch (error) {
+      console.log('in the catch');
+      return false;
     }
-    let jsonifiedResponse;
-     if (response.ok && response.status === 200) {
-       jsonifiedResponse = await response.json();
-     } else {
-       jsonifiedResponse = false;
-     }
-     return jsonifiedResponse;
-   } catch (error) {
-     console.log('in the catch');
-     return false;
-   }
+    let amountWorth;
+    response.forEach
   }
-  function howMany(userMoneyAmount, currencyToConvertTo){
-    let convertedMoney = userMoneyAmount * currencyToConvertTo;
-    return convertedMoney;
+  converter(dollarAmount, convertTo) {
+    this.dollarAmount = dollarAmount;
+    this.convertTo = convertTo;
   }
+
+  amountWorth(dollarAmount, convertTo) {
+    let converted = (dollarAmount * convertTo).toFixed([2]);
+    return converted;
+  }
+
+  
+
+}
+
+export function getMatchingKey(inputKey, response) {
+  
 }
--- src/main.js
+++ src/main.js
@@ -2,22 +2,32 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
-import { exchangeRate } from './src/currency-calculator.js';
+import { ExchangeRate } from './../src/currency-calculator.js';
 
 $(document).ready(function() {
   event.preventDefault();
-  $('#calculate').click(function) {
-    let currencyToConvertTo = ;
+  $('#calculator').click(function () {
+    //let currencyToConvertTo = ;
     (async() => {
-      let exchangeRate = new exchangeRate();
+      let exchangeRate = new ExchangeRate();
       const response = await exchangeRate.getUSDExchangeRate();
       console.log(response);
       getElements(response);
     });
+    
+
+    let checkedVal = $("input[type='radio']:checked").val();
+    let amount = parseInt($("input[name='moneyAmount']").val());
+    
+
+
     function getElements(response) {
-      if(response) {
-        $('#showResponse').html(``)
+      
+      if (response) {
+        $('#convertedMoney').text(`Here is the response: ${data.conversion_rates[1]}`);
+      } else {
+        $('#convertedMoney').text(`Please check your inputs and try again.`);
       }
     }
-  }
-})
+  });
+});
